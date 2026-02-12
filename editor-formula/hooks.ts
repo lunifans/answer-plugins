f1bb66890e908f227d46f693ea7092657a5511c2
Lunifans <lunifans@outlook.com>
2026-02-12 12:00:12 +0800

    Update hooks.ts

--- editor-formula/hooks.ts
+++ editor-formula/hooks.ts
@@ -26,21 +26,11 @@ const useRenderFormula = (element: HTMLElement | RefObject<HTMLElement> | null) 
   const render = (element) => {
     katexRender(element, {
       delimiters: [
-        { left: '$$', right: '$$', display: true },
-        { left: '$$<br>', right: '<br>$$', display: true },
-        {
-          left: '\\begin{equation}',
-          right: '\\end{equation}',
-          display: true,
-        },
-        { left: '\\begin{align}', right: '\\end{align}', display: true },
-        { left: '\\begin{alignat}', right: '\\end{alignat}', display: true },
-        { left: '\\begin{gather}', right: '\\end{gather}', display: true },
-        { left: '\\(', right: '\\)', display: false },
-        { left: '\\[', right: '\\]', display: true },
-        // ★ 在这里加入这一行（注意逗号）
-    { left: '$', right: '$', display: false },
-      ],
+      { left: '$$', right: '$$', display: true },
+      { left: '$', right: '$', display: false },
+      { left: '\\(', right: '\\)', display: false },
+      { left: '\\[', right: '\\]', display: true }
+    ],
       throwOnError: false,
     });
   };
